4a9986e3412245942af85d71b53fed1fe4840c47
320-18\thinkodia <ejjang2030@gmail.com>
2026-02-13 15:44:13 +0900

    chore : vercel 설정 4

--- api/index.py
+++ api/index.py
@@ -1,10 +1,12 @@
-import os
+from flask import Flask
 import sys
+import os
 
-# 현재 위치(api 폴더)의 부모(루트)를 경로에 추가해서 LMS 폴더를 찾을 수 있게 함
+# 현재 위치(api)의 부모 폴더(루트)를 경로에 추가
+# 그래야 옆방에 있는 LMS 폴더를 찾을 수 있음
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# LMS 폴더 안의 app.py에서 flask 객체(app)를 가져옴
+# LMS 폴더 안의 app.py에서 'app' 객체 가져오기
 from LMS.app import app
 
-# Vercel은 이 'app' 객체를 찾아서 실행합니다.
+# Vercel은 이 파일 안에 있는 'app' 변수를 찾아서 실행합니다.
